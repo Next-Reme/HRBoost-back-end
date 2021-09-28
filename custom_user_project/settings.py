439bf3683b11180588a133a460c48ead3b348cef
Omardyab <omarzadyab@gmail.com>
2021-09-28 13:59:34 +0300

    feat:creat user hr signup

--- custom_user_project/settings.py
+++ custom_user_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 'rest_auth.registration',
     'users',
+    
 ]
 
 MIDDLEWARE = [
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
+from .models import CustomUser
 # Create your tests here.
+
+class User_HRTests(TestCase):
+    def setUp(self):
+        self.users = get_user_model().objects.create_user(
+            username="HRBoost", email="HRBoost@outlook.com", password="HRBoost1234"
+        )
+
+    def test_user_create(self):
+        email = self.users.email
+        self.assertEquals(self.users.username.__str__(), "HRBoost")
+        self.assertEquals(self.users.email.__str__(), "HRBoost@outlook.com")
+
+    def test_duplicates(self):
+        try:
+            self.user = get_user_model().objects.create_user(
+                username="HRBoost", email="HRBoost@outlook.com", password="HRBoost1234"
+            )
+        except:
+            print("something wrong, this is a duplicated ")
+
+    def test_none_user_creation(self):
+        User = get_user_model()
+        with self.assertRaises(Exception):
+            User.objects.create_user(email='', password="HRBoost1234")
