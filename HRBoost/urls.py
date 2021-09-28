61e13e90fc7bd5cb3ed223bc905265d608bc7c9d
Ali Rahhal <arahal81@gmail.com>
2021-09-28 20:12:50 +0300

    add user UserVacation edit

--- HRBoost/urls.py
+++ HRBoost/urls.py
@@ -7,12 +7,16 @@ from .views import (
     UserInfoList,
     RolePermissionList,
     UserRoleper,
+    UserVacationDetail,
 )
 
 urlpatterns = [
     path("users/<int:pk>/", UserInfoDetail.as_view(), name="user_info_detail"),
     path("users/", UserInfoList.as_view(), name="user_info_list"),
     path("vacations/", UserVacationList.as_view(), name="user_vacation_list"),
+    path(
+        "vacations/<int:pk>/", UserVacationDetail.as_view(), name="user_vacation_list"
+    ),
     path("attendancelist/", AttendanceList.as_view(), name="attendance_list"),
     path(
         "attendance/<int:user_id>/",
--- HRBoost/users.py
+++ HRBoost/users.py
@@ -1,54 +1,54 @@
-from django.db import models
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+# from django.db import models
+# from django.utils import timezone
+# from django.utils.translation import gettext_lazy as _
+# from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 
-class CustomUser(BaseUserManager):
+# class CustomUser(BaseUserManager):
 
-    def create_superuser(self, email, user_name, first_name, password, **other_fields):
+#     def create_superuser(self, email, user_name, first_name, password, **other_fields):
 
-        other_fields.setdefault('is_HR', True)
-        other_fields.setdefault('is_superuser', True)
-        other_fields.setdefault('is_employee', True)
+#         other_fields.setdefault('is_HR', True)
+#         other_fields.setdefault('is_superuser', True)
+#         other_fields.setdefault('is_employee', True)
 
-        if other_fields.get('is_HR') is not True:
-            raise ValueError(
-                'Superuser must be assigned to is_HR=True.')
-        if other_fields.get('is_superuser') is not True:
-            raise ValueError(
-                'Superuser must be assigned to is_superuser=True.')
+#         if other_fields.get('is_HR') is not True:
+#             raise ValueError(
+#                 'Superuser must be assigned to is_HR=True.')
+#         if other_fields.get('is_superuser') is not True:
+#             raise ValueError(
+#                 'Superuser must be assigned to is_superuser=True.')
 
-        return self.create_user(email, user_name, first_name, password, **other_fields)
+#         return self.create_user(email, user_name, first_name, password, **other_fields)
 
-    def create_user(self, email, user_name, first_name, password, **other_fields):
+#     def create_user(self, email, user_name, first_name, password, **other_fields):
 
-        if not email:
-            raise ValueError(_('You must provide an email address'))
+#         if not email:
+#             raise ValueError(_('You must provide an email address'))
 
-        email = self.normalize_email(email)
-        user = self.model(email=email, user_name=user_name,
-                          first_name=first_name, **other_fields)
-        user.set_password(password)
-        user.save()
-        return user
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, user_name=user_name,
+#                           first_name=first_name, **other_fields)
+#         user.set_password(password)
+#         user.save()
+#         return user
 
 
-class NewUser(AbstractBaseUser, PermissionsMixin):
+# class NewUser(AbstractBaseUser, PermissionsMixin):
 
-    email = models.EmailField(_('email address'), unique=True)
-    user_name = models.CharField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=150, blank=True)
-    start_date = models.DateTimeField(default=timezone.now)
-    about = models.TextField(_(
-        'about'), max_length=500, blank=True)
-    is_HR = models.BooleanField(default=False)
-    is_employee = models.BooleanField(default=False)
+#     email = models.EmailField(_('email address'), unique=True)
+#     user_name = models.CharField(max_length=150, unique=True)
+#     first_name = models.CharField(max_length=150, blank=True)
+#     start_date = models.DateTimeField(default=timezone.now)
+#     about = models.TextField(_(
+#         'about'), max_length=500, blank=True)
+#     is_HR = models.BooleanField(default=False)
+#     is_employee = models.BooleanField(default=False)
 
-    objects = CustomUser()
+#     objects = CustomUser()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['user_name', 'first_name']
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['user_name', 'first_name']
 
-    def __str__(self):
-        return self.user_name
+#     def __str__(self):
+#         return self.user_name
