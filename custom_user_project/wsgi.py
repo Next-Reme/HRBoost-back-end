b2ec9aac6e6a5414f678e1bafb31f7b8b43496e3
Omardyab <omarzadyab@gmail.com>
2021-09-28 03:15:54 +0300

    feat: user and HR auth

--- custom_user_project/wsgi.py
+++ custom_user_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for profok project.
+WSGI config for custom_user_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profok.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'custom_user_project.settings')
 
 application = get_wsgi_application()
